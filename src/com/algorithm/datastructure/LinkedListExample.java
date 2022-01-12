332421c67d8b4b47deff2479620a2df7f226e8bf
phb0228 <phb0228@naver.com>
2022-01-12 14:46:23 +0900

    불필요한 기본 생성자를 제거하라

--- src/com/algorithm/datastructure/LinkedListExample.java
+++ src/com/algorithm/datastructure/LinkedListExample.java
@@ -4,11 +4,6 @@ class Node {
   private String data;
   public Node link;
 
-  public Node() {
-    this.data = null;
-    this.link = null;
-  }
-
   public Node(String data) {
     this.data = data;
     this.link = null;
