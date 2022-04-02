3761be885d1349e59cd30b3ad0826c2f8679b385
박한빈 <phb0228@naver.com>
2022-04-02 17:10:04 +0900

    유효한 팰린드롬 문제

---
+++ src/com/algorithm/example/string/ValidPalindrome.java
@@ -0,0 +1,54 @@
+package com.algorithm.example.string;
+
+import java.util.Scanner;
+
+/**
+ * 설명
+ * 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
+ * 문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+ * 단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
+ * 알파벳 이외의 문자들의 무시합니다.
+ *
+ *
+ * 입력
+ * 첫 줄에 길이 100을 넘지 않는 공백이 없는 문자열이 주어집니다.
+ *
+ * 출력
+ * 첫 번째 줄에 팰린드롬인지의 결과를 YES 또는 NO로 출력합니다.
+ *
+ * 예시 입력 1
+ * found7, time: study; Yduts; emit, 7Dnuof
+ * 예시 출력 1
+ * YES
+ */
+public class ValidPalindrome {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String content = sc.nextLine();
+
+    System.out.println(solution(content));
+  }
+
+  private static String solution(String content) {
+    String contentUpperCase = content.toUpperCase();
+    int left = 0;
+    int right = contentUpperCase.length() - 1;
+    char[] contentArr = contentUpperCase.toCharArray();
+
+    while (left < right) {
+      if (!Character.isAlphabetic(contentArr[left])) {
+        left++;
+      } else if (!Character.isAlphabetic(contentArr[right])) {
+        right--;
+      } else {
+        char tmp = contentArr[left];
+        contentArr[left] = contentArr[right];
+        contentArr[right] = tmp;
+        left++;
+        right--;
+      }
+
+    }
+    return contentUpperCase.equals(String.valueOf(contentArr)) ? "YES" : "NO";
+  }
+}
